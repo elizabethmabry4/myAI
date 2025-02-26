3cd28f9c42d288b8624c41f265cd310e1548027f
elizabethmabry4 <elizabethmabry4@gmail.com>
2025-02-26 13:50:39 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Elizabeth`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Mr. Knightley`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
