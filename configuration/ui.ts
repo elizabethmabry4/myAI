d4f29a8aba336ad0b568d86c67a1fc7a7cdf2bb1
elizabethmabry4 <elizabethmabry4@gmail.com>
2025-02-26 22:58:39 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Jane Austen Expert`;
+export const CHAT_HEADER: string = `AustenBot`;
 export const MESSAGE_PLACEHOLDER: string = `Where was Jane Austen born?`;
 export const FOOTER_MESSAGE: string = `Copyright 2025 by Elizabeth Mabry`;
 export const CLEAR_BUTTON_TEXT: string = `Erase your query`;
-export const PAGE_TITLE: string = `Jane Austen Expert`;
+export const PAGE_TITLE: string = `AustenBot`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
